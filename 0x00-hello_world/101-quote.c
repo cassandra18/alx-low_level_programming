cc90d3db3873132f269021b3ea91ebcac879d300
Cassandra <cassandralelei013@gmail.com>
2023-04-20 20:10:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - a C program that prints a new line to the standard error.
- * Return: 1 (Not Succcess)
+ * Return 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
