404dead33f6934a0863167a3f323ecffb34359b9
Cassandra <cassandralelei013@gmail.com>
2023-04-20 21:28:07 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A C program that prints the size of various types on the computer it is compiled and run on
+ * main - print out sizes of data in C
  * Return: 0 (success)
  */
 int main(void)
