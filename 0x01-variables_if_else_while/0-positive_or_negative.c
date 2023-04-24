b00080a096781ae1bbcaa15a30ad45e0a59876cf
Cassandra <cassandralelei013@gmail.com>
2023-04-24 11:05:45 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 /**
- * main -  print whether the number stored in
- * the variable n is positive or negative
- * Return: 0
-*/
+ * main -print whether the number stored in
+ *  the variable n is positive or negative
+ *  Return: 0
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -17,7 +18,6 @@ int main(void)
 	if (n > 0)
 	{
 		return (is positive);
-
 	}
 	else if (n == 0)
 	{
