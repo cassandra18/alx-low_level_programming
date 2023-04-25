35d9c19d538d609838dfab2a0aff3e8073c157ae
Cassandra <cassandralelei013@gmail.com>
2023-04-25 03:56:38 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints all single digit
+ * numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
