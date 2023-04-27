329cb683a4f0acab292065bc42936f401439f71a
Cassandra <cassandralelei013@gmail.com>
2023-04-27 07:51:10 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two digits
+ * @a: first number
+ * @b: second number
+ * Return: the result of multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
