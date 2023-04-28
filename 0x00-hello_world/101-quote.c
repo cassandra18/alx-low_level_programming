2bccf7aa14d490827a1b8d5a81075d911a05cd83
Cassandra <cassandralelei013@gmail.com>
2023-04-28 04:08:05 -0700

    0-main.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints a line to the standard error
- * Return : 1
+ * main - Entry point
+ * Return: 1
  */
 int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-The alx-low_level_programming repository contains files that contains answers to the alx 0x00.c-Hello,World tasks
+0x03. C - Debugging
