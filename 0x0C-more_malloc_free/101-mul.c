5177f83576df2b92fddfdeb817afd0a29d25274b
Cassandra <cassandralelei013@gmail.com>
2023-05-19 09:15:33 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,52 +5,121 @@
  * is_valid_number - checks if a given string is composed only of digits
  * @num: the given string to be checked
  * Return: 0 if false, 1 if true
- * multiply - return product of two integers
- * @num1: the first integer
- * @num2: the second integer
- * Return: product
- * main -  check if the number of arguments (argc) is not equal to 3
- * Return: status of 98
+ *
  */
 int is_valid_number(char *num)
 {
 	while (*num)
 	{
-		if (!isdigit(*num))
-		{
-			return (0);
-		}
-		num++;
+	if (!isdigit(*num))
+	{
+	return (0);
+	}
+	num++;
 	}
 	return (1);
 }
 
-	unsigned long long multiply(unsigned long long num1, unsigned long long num2)
+/**
+ * multiply - return product of two integers
+ * @num1: the first integer
+ * @num2: the second integer
+ * @result: product
+ * Return: product
+ */
+int multiply(const char *num1, const char *num2, char *result)
+{
+	int *intermid_result;
+	int i, j, product, pos1, pos2, sum, index, start;
+	int len1 = 0;
+	int len2 = 0;
+
+	while (num1[len1] != '\0')
+	len1++;
+
+	while (num2[len2] != '\0')
+	len2++;
+
+	intermid_result = malloc(sizeof(int) * (len1 + len2));
+
+	if (intermid_result == NULL)
+	return (0);
+
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		return (num1 * num2);
+	for (j = len2 - 1; j >= 0; j--)
+	{
+	product = (num1[i] - '0') * (num2[j] - '0');
+	pos1 = i + j;
+	pos2 = i + j + 1;
+	sum = product + intermid_result[pos2];
+
+	intermid_result[pos1] += sum / 10;
+	intermid_result[pos2] = sum % 10;
+	}
 	}
 
-	int main(int argc, char *argv[])
+	index = 0;
+	start = 0;
+
+	while (index < len1 + len2)
 	{
-		if (argc != 3)
-		{
-			printf("Error\n");
-			return (98);
-		}
+	if (intermid_result[index] != 0)
+	start = 1;
 
-		char *num1_str = argv[1];
-		char *num2_str = argv[2];
-
-		if (!is_valid_number(num1_str) || !is_valid_number(num2_str))
-		{
-			printf("Error\n");
-			return (98);
-		}
-		
-		unsigned long long num1 = strtoull(num1_str, NULL, 10);
-		unsigned long long num2 = strtoull(num2_str, NULL, 10);
-		unsigned long long result = multiply(num1, num2);
-
-		printf("%ull\n", result);
-		return (0);
+	if (start)
+	{
+	result[index] = intermid_result[index] + '0';
+	index++;
+			}
 	}
+
+	result[index] = '\0';
+
+	free(intermid_result);
+	return (1);
+}
+
+/**
+ * main -  check if the number of arguments (argc) is not equal to 3
+ * @argc:  argument count
+ * @argv: argument vector
+ * Return: status of 98
+ */
+int main(int argc, char *argv[])
+{
+	char *num1;
+	char *num2;
+	int index;
+	char result[256];
+
+	if (argc != 3)
+	{
+	_putchar(Error);
+	_putchar('\n');
+	return (98);
+	}
+
+	num1 = argv[1];
+	num2 = argv[2];
+
+	if (!is_valid_number(num1_str) || !is_valid_number(num2_str))
+	{
+	_putchar(Error);
+	_putchar('\n');
+	return (98);
+	}
+
+	if (multiply(num1, num2, result))
+	{
+	index = o;
+	while (result[index] != '\0')
+	{
+	_putchar(result[index]);
+	index++;
+	}
+	_putchar('\n');
+	}
+
+	return (0);
+}
