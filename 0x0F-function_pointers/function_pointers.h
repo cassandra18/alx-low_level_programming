c7a828014030597ff6540ecdd2d7e4d10966d115
Cassandra <cassandralelei013@gmail.com>
2023-05-24 12:35:06 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,9 +4,4 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-int (get_op_func(op)(num1, num2);
-int op_mod(int a, int b);
-int op_add(int a, int b);
-int _putchar(char c);
 #endif /*FUNCTION_HEADERS_H*/
