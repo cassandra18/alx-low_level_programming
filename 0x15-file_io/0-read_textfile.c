b212cca1f3dc1a8a4631924b4ae63c8257d4709e
Cassandra <cassandralelei013@gmail.com>
2023-06-12 23:42:46 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,63 +1,48 @@
 #include "main.h"
 
 /**
- * read_textfile - a function that reads a text file and
- * prints it to the POSIX standard output.
- * @filename: the file to be read
- * @letters: is the number of letters it should read and print
- * Return: if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
- */
+* read_textfile - a function that reads a text file and
+* prints it to the POSIX standard output.
+* @filename: the file to be read
+* @letters: is the number of letters it should read and print
+* Return: if the file can not be opened or read, return 0
+* if filename is NULL return 0
+* if write fails or does not write the expected amount of bytes, return 0
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fileDescriptor;
-	char *buffer;
-	ssize_t read_bytes;
-	ssize_t written_bytes;
+ssize_t open_file;
+char *buffer;
+ssize_t read_bytes;
+ssize_t written_bytes;
 
 
-	if (filename == NULL)
-	{
-		return (0);
-	}
+if (filename == NULL)
+{
+return (0);
+}
 
-	fileDescriptor = fopen(filename, "r");
-	if (fileDescriptor == NULL)
-	{
-		return (0);
-	}
-
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-	{
-		fclose(fileDescriptor);
-		return (0);
-	}
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+return (0);
+}
 
 
-	read_bytes = fread(buffer, sizeof(char), letters, fileDescriptor);
+open_file = open(filename, O_RDONLY);
+read_bytes = read(open_file, buffer, letters);
+written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
 
-	if (read_bytes < 0)
-	{
-		fclose(fileDescriptor);
-		free(buffer);
-		return (0);
-	}
+if (open_file == -1 || read_bytes == -1 || written_bytes == -1 ||
+written_bytes != read_bytes)
+{
 
-	buffer[read_bytes] = '\0';
+free(buffer);
+return (0);
+}
 
+free(buffer);
+close(open_file);
 
-	written_bytes = fwrite(buffer, sizeof(char), read_bytes, stdout);
-
-	if (written_bytes < read_bytes)
-	{
-		fclose(fileDescriptor);
-		free(buffer);
-		return (0);
-	}
-
-	fclose(fileDescriptor);
-	free(buffer);
-	return (written_bytes);
+return (written_bytes);
 }
