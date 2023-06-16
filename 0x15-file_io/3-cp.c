9d064092df8b2627feff55b92b92015a5efbae9c
Cassandra <cassandralelei013@gmail.com>
2023-06-15 23:58:10 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,90 +1,67 @@
 #include "main.h"
 
-
 /**
-* create_buffer - allocates 1024 bytes for a buffer
-* @file: the file whose chars are being stored by the bufer
-* Return: pointer to the buffer
-*/
-
-char *create_buffer(char *file)
+ * handle_error - prints error message to the standard input
+ * @message: the message to print
+ * @number: the status code to exit
+ * @filename: the file name
+ */
+ 
+void handle_error(const char *message, const char *filename, int number)
 {
-char *buffer;
-
-buffer = malloc(sizeof(char) * BUFFER_SIZE);
-
-if (buffer == NULL)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't write to %s\n", file);
-exit(99);
-}
-return (buffer);
+	if (filename != NULL)
+		dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+	else
+		dprintf(STDERR_FILENO, "%s\n", message);
+	exit(number);
 }
 
 /**
-* close_file - closes file descriptors
-* @fd: the file descriptors to be closed
-*/
-void close_file(int fd)
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ *
+ */
+
+int main(int argc, char **argv)
 {
-int closer;
+	int fdFrom, fdTo;
+	ssize_t readBytes, writtenBytes;
+	char buffer[BUFFER_SIZE];
 
-closer = close(fd);
+	if (argc != 3)
+		handle_error("ERROR: cp file_from", "file_to", 97);
+	fdFrom = open(argv[1], O_RDONLY);
+	if (fdFrom == -1)
+		handle_error("Error: Can't read from file", argv[1], 98);
+	fdTo = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fdTo == -1)
+		handle_error("Error: Can't write to", argv[2], 99);
+	while ((readBytes = read(fdFrom, buffer, BUFFER_SIZE)) > 0)
+	{
+		writtenBytes = write(fdTo, buffer, readBytes);
+		if (writtenBytes == -1 || writtenBytes != readBytes)
+		{
+			close(fdFrom);
+			close(fdTo);
+			handle_error("Error: Can't write to", argv[2], 99);
+		}
+	}
 
-if (closer == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-}
-
-/**
-* main - copy contents of a file to another file
-* @argc: argument count
-* @argv: argument vector
-* Return: on success return 0
-*/
-int main(int argc, char *argv[])
-{
-int from_file, to_file, readto, writeto;
-char *buffer;
-
-if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
-
-buffer = create_buffer(argv[2]);
-from_file = open(argv[1], O_RDONLY);
-readto = read(from_file, buffer, BUFFER_SIZE);
-to_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-do {
-if (from_file == -1 || readto == -1)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't read from file %s\n", argv[1]);
-free(buffer);
-exit(98);
-}
-
-writeto = write(to_file, buffer, readto);
-if (to_file == -1 || writeto == -1)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't read from file %s\n", argv[2]);
-free(buffer);
-exit(99);
-}
-readto = read(from_file, buffer, BUFFER_SIZE);
-to_file = open(argv[2], O_WRONLY | O_APPEND);
-} while (readto > 0);
-
-free(buffer);
-close_file(from_file);
-close_file(to_file);
-
-return (0);
+	if (readBytes == -1)
+	{
+		close(fdFrom);
+		close(fdTo);
+		handle_error("Error: Can't read from file", argv[1], 98);
+	}
+	if (close(fdFrom) == -1)
+	{
+		handle_error("Error: Can't close fd", "fdFrom", 100);
+	}
+	else if (close(fdTo) == -1)
+	{
+		handle_error("Error: Can't close fd", "fdTo", 100);
+	}
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,7 +14,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void handleError(const char *error_message, const char *file_name);
+void handle_error(const char *message, const char *filename, int number);
 void print_elf_header_info(const char *elf_filename);
 int _strlen(char *str);
 void close_file(int fd);
