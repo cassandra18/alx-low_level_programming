c122ad6d4a1562f97358de2edea010167a16d3db
Cassandra <cassandralelei013@gmail.com>
2023-07-24 02:01:24 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - function that performs addition
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int add(int a, int b)
+{
+return a + b;
+}
+
+/**
+* sub - function that performs subtraction
+* @a: first number
+* @b: second number
+* Return: difference
+*/
+int sub(int a, int b)
+{
+return a - b;
+}
+
+/**
+* mul - function that performs multiplication
+* @a: first number
+* @b: second number
+* Return: product
+*/
+int mul(int a, int b)
+{
+return a * b;
+}
+
+/**
+* div - function that performsdivision
+* @a: first number
+* @b: second number
+* Return: quotient
+*/
+int div(int a, int b)
+{
+return a / b;
+}
+
+/**
+* add - function that performs modulus
+* @a: first number
+* @b: second number
+* Return: remainder
+*/
+int mod(int a, int b)
+{
+return a % b;
+}
